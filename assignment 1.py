d11595469c4aaf292474822f320c6686f2af8839
akshatja07 <akshonline02@gmail.com>
2025-11-06 02:41:03 +0530

    commit

---
+++ assignment 1.py
@@ -0,0 +1,26 @@
+# Take two number as input from user
+num1=float(input("enter your first number:"))
+num2=float(input("enter your second number:"))
+# perform basic mathmatic operation on this two number
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+# display result on the screen
+print("\nresult")
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
+
+# Takes a user's first name and last name as input.
+print("\n")
+print("\n")
+
+first_name = input("enter your first name:")
+last_name = input("enter your last name:")
+
+# Concatenates the first name and last name into a full name.
+full_name = first_name +" " +last_name
+#Prints a personalized greeting message using the full name.
+print('hello,',full_name,"! Welcome to the python program")
